5868d4abc798f0c828a4a0f5eafcbf751cbf23d8
juwon-code <juwoncode@gmail.com>
2023-12-22 15:25:36 +0900

    feat: 공통 컴포넌트 추가
    - 로고 생성 및 부트스트랩 임포트.
    - 임시 로그인 페이지, 컨트롤러 매핑.
    - 공통 컴포넌트(헤드, 헤더, 푸터)  구현.
    
    Related to: #5

---
+++ src/main/java/juwoncode/commonblogproject/controller/MemberController.java
@@ -0,0 +1,19 @@
+package juwoncode.commonblogproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "member/login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "member/register";
+    }
+}
